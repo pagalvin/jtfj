768282f9e454ed8d6eef5dd6295f8ad56acc98f2
Paul Galvin <paul.galvin@slalom.com>
2017-02-23 08:17:21 -0500

    Copied over from the ng1 project but they are not yet converted.

---
+++ app/Admin/QuizTemplates/ListQuizTemplatesController.ts
@@ -0,0 +1,50 @@
+﻿module jtfj.Admin.controllers {
+
+    import interfaces = jtfj.Interfaces;
+    import F = jtfj.Framework.Functionals;
+
+    export class QuizTemplatesController {
+
+        static $inject = [
+            "$q",
+            "$log",
+            "jtfj.Admin.Services.QuizTemplatesService"
+        ];
+
+        
+        public get AllQuizTemplates() { return this._quizTemplatesService.AllQuizTemplates; }
+
+        constructor(private $q: ng.IQService,
+            private $log: ng.ILogService,
+            private _quizTemplatesService: jtfj.Admin.Services.QuizTemplatesService) {
+
+            this.$log.debug(`QuizTemplatesController: ctor: Entering.`);
+
+            this._quizTemplatesService.LoadAllQuizTemplates().then(
+                (results) => {
+                    this.$log.debug(`ListQuizTemplateController: Loaded quiz templates:`, this.AllQuizTemplates);
+                },
+                (errorDetails) => {
+                }
+            );
+
+        } // constructor
+
+        public Ping(): void {
+            this._quizTemplatesService.Ping();
+        }
+
+        /**
+         * Returns a comma delimited list of knowledge domains suitable for rendering on the UI.
+         * @param forDomains: array of knowledge domain items.
+         */
+        public GetFriendlyKDDisplay(forDomains: Entities.KnowledgeDomainItem[]) {
+
+            return F.stringArrayToCdl(F.extractFieldsFromCollection<string[]>(forDomains, "Title"));
+        }
+    }
+
+    angular
+        .module('jtfj')
+        .controller("jtfj.Admin.controllers.QuizTemplatesController", QuizTemplatesController);
+}
---
+++ app/EndUser/QuizInstances/QuizInstanceItem.ts
@@ -0,0 +1,43 @@
+﻿module jtfj.Entities {
+
+    import framework = jtfj.Framework;
+    export interface IQuizInstanceItem extends jtfj.Framework.Entities.IAbstractItem {
+        Title: string;
+        Description: string;
+        KnowledgeDomains: jtfj.Entities.KnowledgeDomainItem[];
+        Facts: IFactItem[];
+    }
+
+    export class QuizInstanceItem extends framework.Entities.AbstractItem implements IQuizInstanceItem{
+
+        public Title: string;
+        public Description: string;
+        public KnowledgeDomains: KnowledgeDomainItem[];
+        public Facts: IFactItem[];
+
+        constructor() {
+            super();
+
+            this.Title = "";
+            this.UniqueID = "";
+            this.Description = "";
+            this.KnowledgeDomains = [];
+            this.Facts = [];
+        }
+
+        static MakeSafe(QuizInstanceItem: QuizInstanceItem): QuizInstanceItem {
+
+            const result = new jtfj.Entities.QuizInstanceItem();
+            result.Description = QuizInstanceItem.Description || "";
+            result.Title = QuizInstanceItem.Title || "";
+            result.UniqueID = QuizInstanceItem.UniqueID || "";
+            result.Facts = QuizInstanceItem.Facts || [];
+            result.KnowledgeDomains = QuizInstanceItem.KnowledgeDomains || [];
+
+            return result;
+
+        }
+
+    }
+
+}
